5c7eb86d5984e194a23987b34db0c19e1bd22af8
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-01-17 20:52:31 +0530

    Update ColorCoding.c

--- ColorCoding.c
+++ ColorCoding.c
@@ -33,3 +33,14 @@ int GetPairNumber(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
+
+void GetReferenceManual(){
+    printf("Color Pair Manual:\n PairNumber MajorColor MinorColor\n");
+    for (int pairNumber=0; pairNumber<MAX_COLORPAIR; pairNumber++)
+    {
+         ColorPair colorPair = GetColorPair(pairNumber);
+         char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+         ColorPairString(&colorPair, colorPairNames);
+         printf("%d %s\n", pairNumber, colorPairNames);
+    }
+}
