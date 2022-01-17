7b674e1bf62e57e492e33004475c9115ed5344b3
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-01-17 08:03:48 +0530

    Added function declarations in the header file

--- ColorCoding.h
+++ ColorCoding.h
@@ -23,3 +23,9 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) ;
+
+ColorPair GetColorFromPairNumber(int pairNumber);
+
+int GetPairNumberFromColor(const ColorPair* colorPair) ;
