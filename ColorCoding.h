ad79f203f9506220224e0931b6caeedcec1680ec
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-01-18 06:48:48 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -9,10 +9,8 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 extern const char* MajorColorNames[];
 extern const char* MinorColorNames[];
 
-
 extern int numberOfMajorColors; 
 extern int numberOfMinorColors;
-
 
 typedef struct {
     enum MajorColor majorColor;
